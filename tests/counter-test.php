2350b35c67ff4dc266aaa72729eae1ebbfe2880f
Francesco Bianco <info.francescobianco@gmail.com>
2022-02-01 19:13:41 +0100

    First commit

--- tests/counter-test.php
+++ tests/counter-test.php
@@ -1,9 +1,9 @@
 <?php
 
-$validFor =
-$token = md5(intdiv(time(), 10));
+$validFor = 10;
+$token = md5(intdiv(time(), $validFor));
 for ($i = 0; $i < 30; $i++) {
-    $seed = intdiv(time(), 10);
+    $seed = intdiv(time(), $validFor);
 
     $valid = 'no';
     if ($token == md5($seed) || $token == md5($seed - 1)) {
